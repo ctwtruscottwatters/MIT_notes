17aa05ba6d24bb4ab605c6976cab6b2c60ef2c02
Charles Truscott Watters <87652332+ctwtruscottwatters@users.noreply.github.com>
2022-03-23 07:47:52 +1100

    Update tannewtonraphson.py

--- tannewtonraphson.py
+++ tannewtonraphson.py
@@ -18,5 +18,13 @@ def main():
     print('tan({}) is the same as tan({})'.format(shout_out_to_MIT, guess))
     print('which are {} and {}'.format(y, numpy.tan(guess)))
     
-    
+"""
+0.49999999999999994 is the guess and it has taken 1 iterations
+0.8494156605301215 is the guess and it has taken 2 iterations
+0.7896655706082891 is the guess and it has taken 3 iterations
+0.785416425859517 is the guess and it has taken 4 iterations
+tan(0.7853981633974483) is the same as tan(0.7853981637309698)
+which are 0.9999999999999999 and 1.000000000667043
+"""
+
 if __name__ == "__main__": main()
