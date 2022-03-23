5328e9a69321a835fda38510c6cfe93466645144
Charles Truscott Watters <87652332+ctwtruscottwatters@users.noreply.github.com>
2022-03-23 14:51:57 +1100

    Add files via upload

--- MIT_test_find_root_Charles_Truscott.py
+++ MIT_test_find_root_Charles_Truscott.py
@@ -6,7 +6,9 @@
 # Nice to review source code and find every operation to describe
 # Eventually hope to learn optimisation, simulation and statistica; modelling in the second inaugural CSci unit from MIT
 # Note taking and 5 hours of study a day for 6.001 and 6.002, should pass :-)
-
+# Second Revision, Third and Sixth and Ninety-Ninth roots needed to reach the sine, cosine and tangent of 45 degrees (invoked in the function in radians)
+# going to work on this one
+import numpy as np
 def find_root(x, power, epsilon):
     if x < 0 and power % 2 == 0:
         return None
@@ -32,74 +34,23 @@ def test_find_root(x_vals, powers, epsilons):
                     val = 'Okay'
                     if abs(result ** p - x) > e:
                         val = 'Bad'
-                print('f\'x = {}, power = {}, epsilon = {}, val = {}, result = {}'.format(x, p, e, val, result))
                 if result != None:
-                    print('{} ^ {} = {}'.format(result, p, x))
+                    if x == float(np.sin(45.0 * np.pi / 180.0)):
+                        print('{} ^ {} = {} which is sin({}) {} {} {}'.format(result, p, x, '45.0', 'in radians or', (result ** p) * 180.0 / np.pi, ' degrees'))
+                        print('arcsin({} ^ {}) = {} degrees'.format(result, p, np.arcsin(result ** p) * 180 / np.pi))
+                    elif x == float(np.cos(45.0 * (np.pi / 180))):
+                        print('{} ^ {} = {} which is cos({}) {} {} {}'.format(result, p, x, '45.0', 'in radians or', (result ** p) * 180.0 / np.pi, ' degrees'))
+                        print('arccos({} ^ {}) = {} degrees'.format(result, p, np.arccos(result ** p) * 180 / np.pi))
+                    elif x == float(np.tan(45.0 * (np.pi / 180.0))):
+                        print('{} ^ {} = {} which is tan({}) {} {} {}'.format(result, p, x, '45.0', 'in radians or', (result ** p) * 180.0 / np.pi, ' degrees'))
+                        print('arctan({} ^ {}) = {} degrees'.format(result, p, np.arctan(result ** p) * 180 / np.pi))
+                    
 def main():
-    x_vals = (0.50, 0.333, -9)
-    powers = (1, 2, 3)
+    x_vals = (float(np.sin(45.0 * np.pi / 180.0)), float(np.cos(45.0 * (np.pi / 180))), float(np.tan(45.0 * (np.pi / 180.0))))
+    powers = (3, 6, 99)
     epsilons = (0.001, 0.00001, 0.0000001)
     test_find_root(x_vals, powers, epsilons)
     print('Charles Truscott for an MIT certificate, page 70 Guttag et. alia')
     
 if __name__ == "__main__": main()
 
-
-"""
-
-runfile('C:/Users/user/Desktop/MIT_test_find_root_Charles_Truscott.py', wdir='C:/Users/user/Desktop')
-f'x = 0.5, power = 1, epsilon = 0.001, val = Okay, result = 0.5
-0.5 ^ 1 = 0.5
-f'x = 0.5, power = 1, epsilon = 1e-05, val = Okay, result = 0.5
-0.5 ^ 1 = 0.5
-f'x = 0.5, power = 1, epsilon = 1e-07, val = Okay, result = 0.5
-0.5 ^ 1 = 0.5
-f'x = 0.5, power = 2, epsilon = 0.001, val = Okay, result = 0.70703125
-0.70703125 ^ 2 = 0.5
-f'x = 0.5, power = 2, epsilon = 1e-05, val = Okay, result = 0.7071075439453125
-0.7071075439453125 ^ 2 = 0.5
-f'x = 0.5, power = 2, epsilon = 1e-07, val = Okay, result = 0.7071068286895752
-0.7071068286895752 ^ 2 = 0.5
-f'x = 0.5, power = 3, epsilon = 0.001, val = Okay, result = 0.7939453125
-0.7939453125 ^ 3 = 0.5
-f'x = 0.5, power = 3, epsilon = 1e-05, val = Okay, result = 0.793701171875
-0.793701171875 ^ 3 = 0.5
-f'x = 0.5, power = 3, epsilon = 1e-07, val = Okay, result = 0.7937005758285522
-0.7937005758285522 ^ 3 = 0.5
-f'x = 0.333, power = 1, epsilon = 0.001, val = Okay, result = 0.33203125
-0.33203125 ^ 1 = 0.333
-f'x = 0.333, power = 1, epsilon = 1e-05, val = Okay, result = 0.3330078125
-0.3330078125 ^ 1 = 0.333
-f'x = 0.333, power = 1, epsilon = 1e-07, val = Okay, result = 0.33299994468688965
-0.33299994468688965 ^ 1 = 0.333
-f'x = 0.333, power = 2, epsilon = 0.001, val = Okay, result = 0.5771484375
-0.5771484375 ^ 2 = 0.333
-f'x = 0.333, power = 2, epsilon = 1e-05, val = Okay, result = 0.577056884765625
-0.577056884765625 ^ 2 = 0.333
-f'x = 0.333, power = 2, epsilon = 1e-07, val = Okay, result = 0.5770615339279175
-0.5770615339279175 ^ 2 = 0.333
-f'x = 0.333, power = 3, epsilon = 0.001, val = Okay, result = 0.693359375
-0.693359375 ^ 3 = 0.333
-f'x = 0.333, power = 3, epsilon = 1e-05, val = Okay, result = 0.6931304931640625
-0.6931304931640625 ^ 3 = 0.333
-f'x = 0.333, power = 3, epsilon = 1e-07, val = Okay, result = 0.6931300163269043
-0.6931300163269043 ^ 3 = 0.333
-f'x = -9, power = 1, epsilon = 0.001, val = Okay, result = -8.9993896484375
--8.9993896484375 ^ 1 = -9
-f'x = -9, power = 1, epsilon = 1e-05, val = Okay, result = -8.999990463256836
--8.999990463256836 ^ 1 = -9
-f'x = -9, power = 1, epsilon = 1e-07, val = Okay, result = -8.999999925494194
--8.999999925494194 ^ 1 = -9
-f'x = -9, power = 2, epsilon = 0.001, val = No root exists, result = None
-f'x = -9, power = 2, epsilon = 1e-05, val = No root exists, result = None
-f'x = -9, power = 2, epsilon = 1e-07, val = No root exists, result = None
-f'x = -9, power = 3, epsilon = 0.001, val = Okay, result = -2.08013916015625
--2.08013916015625 ^ 3 = -9
-f'x = -9, power = 3, epsilon = 1e-05, val = Okay, result = -2.0800843238830566
--2.0800843238830566 ^ 3 = -9
-f'x = -9, power = 3, epsilon = 1e-07, val = Okay, result = -2.0800838209688663
--2.0800838209688663 ^ 3 = -9
-Charles Truscott for an MIT certificate, page 70 Guttag et. alia
-
-
-"""
